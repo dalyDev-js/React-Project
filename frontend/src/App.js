04f0ed5f050f7603b8d6f0e7c787053ecf9b79a6
Abdulrhman El-Daly <abd911daly@gmail.com>
2024-08-09 01:24:20 +0300

    header, carousel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,18 +8,20 @@ import AllProducts from "./Pages/AllProducts/AllProducts";
 import PageNotFound from "./Pages/PageNotFound/PageNotFound";
 import Home from "./Pages/Home/Home";
 import Header from "./Components/Navbar/Navbar";
+import MyGallery from "./Components/Carousel/Carousel";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Header/>
+        <Header />
         <Routes>
           <Route path="/" Component={Home} />
           <Route path="/signin" Component={SignIn} />
           <Route path="/signup" Component={SignUp} />
           <Route path="/card" Component={Card} />
           <Route path="/products" Component={AllProducts} />
+          <Route path="/carousel" Component={MyGallery} />
           <Route path="*" Component={PageNotFound} />
         </Routes>
 
--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Carousel from "../Carousel/Carousel";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <div className="header-container flex items-center justify-center">
+        <Carousel />
+      </div>
+    </div>
+  );
 }
 
 export default Header;
