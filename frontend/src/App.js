893f674ea8759fc7569575ac84529995a5fb46f8
mohammedhosni25@gmail.com <mohammedhosni25@gmail.com>
2024-08-08 21:09:02 +0300

    header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
 
 function App() {
   return <div className="App">
+    <Header/>
     <Footer/>
   </div>;
 }
