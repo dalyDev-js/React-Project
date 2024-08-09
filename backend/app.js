b4d0c038bd02fa6827ba54438b92cd448061ebf5
Abdulrhman El-Daly <abd911daly@gmail.com>
2024-08-09 22:42:34 +0300

    thunk, auth, cart

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ import { dbConnect } from "./src/db/dbConnect.js";
 import productRoutes from "./src/routes/products.routes.js";
 import userRoutes from "./src/routes/user.routes.js";
 import AppError from "./src/utils/appError.js";
+import cartRoutes from "./src/routes/cart.routes.js";
 
 const app = express();
 const port = 3001;
@@ -12,6 +13,7 @@ dbConnect();
 app.use(express.json());
 app.use(productRoutes);
 app.use(userRoutes);
+app.use(cartRoutes);
 
 app.use("*", (req, res, next) => {
   next(new AppError("URL NOT FOUND", 404));
--- backend/src/controllers/cart.controller.js
+++ backend/src/controllers/cart.controller.js
@@ -1,10 +1,19 @@
-import cartModel from "../models/Cart";
-import catchError from "../middleware/catchError";
+import cartModel from "../models/Cart.js";
+import catchError from "../middleware/catchError.js";
 
 const getCartItems = catchError(async (req, res) => {
-  const userId = req.params.userId;
+  const userId = req.user._id;
   const cartItems = await cartModel.find({ userId });
+  console.log(userId);
   res.status(200).json({ message: "success", cartItems });
 });
 
-export { getCartItems };
+const addCartItems = catchError(async (req, res) => {
+  const userId = req.user._id;
+  req.body.createdBy = userId;
+  req.body.userId = userId;
+  const createdCart = await cartModel.create(req.body);
+  res.status(201).json({ message: "Cart item created", createdCart });
+});
+
+export { getCartItems, addCartItems };
