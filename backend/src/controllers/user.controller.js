2bce0b56253958c75ad736c9b565f851dbbaff62
Abdulrhman El-Daly <abd911daly@gmail.com>
2024-08-13 01:29:14 +0300

    merged

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -53,4 +53,13 @@ const verifyEmail = catchError(async (req, res, next) => {
   });
 });
 
+// exports.getAllUsers = async (req, res) => {
+//   try {
+//     const users = await User.find();
+//     res.status(200).json(users);
+//   } catch (error) {
+//     res.status(500).json({ message: "Failed to retrieve users" });
+//   }
+// };
+
 export { signup, signin, verifyEmail };
