bd94cf99b961120198c5e415fea7fe816225d86f
Abdulrhman El-Daly <67233903+dalyDev-js@users.noreply.github.com>
2024-10-10 13:12:46 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,20 +6,21 @@ import userRoutes from "./src/routes/user.routes.js";
 import AppError from "./src/utils/appError.js";
 import cartRoutes from "./src/routes/cart.routes.js";
 
-// CORS configuration
-const corsOptions = {
-  origin: ["http://localhost:3000", "https://react-project-production-5a86.up.railway.app"], // Your frontend URLs
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow all necessary HTTP methods
-  credentials: true, // Allow cookies and credentials to be passed
-  allowedHeaders: ["Content-Type", "Authorization"], // Allow necessary headers
-  optionsSuccessStatus: 200, // Response status for successful OPTIONS requests
-};
+ 
 
 const app = express();
 const port = process.env.PORT || 3001;
-app.use(cors(corsOptions));
-// Apply CORS middleware
-app.options("*", cors(corsOptions));
+ app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  // If it's an OPTIONS request, return early
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 dbConnect();
 
