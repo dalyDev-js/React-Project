d18e70a7b84683da480403076c75dec27c52c134
Abdulrhman El-Daly <67233903+dalyDev-js@users.noreply.github.com>
2024-10-10 13:21:32 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,37 +6,26 @@ import userRoutes from "./src/routes/user.routes.js";
 import AppError from "./src/utils/appError.js";
 import cartRoutes from "./src/routes/cart.routes.js";
 
- 
 const corsOptions = {
   origin: ["http://localhost:3000", "https://your-production-frontend.com"], // Add your frontend URLs here
   credentials: true, // If you are using cookies or authentication tokens
 };
+
 const app = express();
-const port = process.env.PORT || 3001;
- 
+const port = 3001;
 app.use(cors(corsOptions));
 dbConnect();
-
-// Middleware to parse JSON
 app.use(express.json());
-
-// Define your routes
 app.use(productRoutes);
 app.use(userRoutes);
 app.use(cartRoutes);
-
-// Root route
 app.get("/", (req, res) => res.send("Hello World!"));
-
-// Catch-all for undefined routes
 app.use("*", (req, res, next) => {
   next(new AppError("URL NOT FOUND", 404));
 });
 
-// Global error handler
 app.use((err, req, res, next) => {
-  res.status(err.statusCode || 500).json({ message: err.message });
+  res.status(err.statusCode).json({ message: err.message });
 });
 
-// Start server
-app.listen(port, () => console.log(`Backend is running on port ${port}`));
+app.listen(port, () => console.log("listening on port ", port));
